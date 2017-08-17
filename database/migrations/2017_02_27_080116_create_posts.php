f0809260a92a7d8220018c4f37953f4e108c8ee8
lzjcoding <lzjcoding@users.noreply.github.com>
2017-08-17 17:38:52 +0800

    Update 2017_02_27_080116_create_posts.php
    fix 运行迁移文件失败

--- database/migrations/2017_02_27_080116_create_posts.php
+++ database/migrations/2017_02_27_080116_create_posts.php
@@ -29,8 +29,6 @@ class CreatePosts extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             $table->softDeletes();
-
-            $table->index('user_id');
         });
     }
 
